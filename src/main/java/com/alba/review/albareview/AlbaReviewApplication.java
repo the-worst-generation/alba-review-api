97ef3232d3112a3e7b90a4988eb14215861cc01b
unknown <yoj9168@gmail.com>
2023-03-13 09:51:03 +0900

    first commit

---
+++ src/main/java/com/alba/review/albareview/AlbaReviewApplication.java
@@ -0,0 +1,12 @@
+package com.alba.review.albareview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlbaReviewApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(AlbaReviewApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alba/review/albareview/AlbaReviewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alba.review.albareview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlbaReviewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
